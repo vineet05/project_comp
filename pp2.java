8e48356dd56dce4022474089034ddd01d388c073
vineet <29vineet07@gmail.com>
2022-05-25 00:57:00 +0530

    test2

--- pp2.java
+++ pp2.java
@@ -2,7 +2,7 @@ public class pp2 {
     public static void main(String[] args) {
         for(int i=0;i<10;i++)//loop for 1 to 10
         {
-            System.out.println(i);//print the loop variable
+            System.out.println(i);//print the loop
         }
     }
 }
