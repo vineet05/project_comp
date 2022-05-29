f7dcac67b6f00016635f9fc7bcfc378da39cb9cd
vineet kumar <86847611+vineet05@users.noreply.github.com>
2022-05-29 13:17:07 +0530

    pp1

--- pp1.java
+++ pp1.java
@@ -5,9 +5,36 @@ class pp1
     {
         Scanner sc =new Scanner(System.in);
         int N=sc.nextInt();//input
-        for(int i=N;i=)
+        String s=Integer.toString(N);
+        int p=N; int c=0;
+        int l=s.length();
+        for(int i=0;i<l;i++)
+        {
+            char r=s.charAt(i);
+            s=s.substring(1,l)+r;
+            System.out.println(s);
+            if(prime(s)==true)
+            c++;
+        }
+        if (c==l)
+        System.out.println(p+"IS A CIRCULAR NUMBER");
         //loop front to back
         //   {//boolean calling}
         //if 
+    }
+    static boolean prime(String x) 
+    {
+        Integer a=Integer.valueOf(x); 
+        int r=0;
+        for(int i=1;i<a;i++)
+        {
+            int j=a%i;
+            if(j==0)
+            r++;
+        }
+        if(r==2)
+        return true;
+        else 
+        return false;
     }
 }
